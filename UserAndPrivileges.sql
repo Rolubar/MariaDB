4c81567f076c90676f77d7fa7a04afa905f50dd8
Rodrigo Luiz Barbosa <rolubar@hotmail.com>
2016-10-14 10:37:46 -0300

    Alter identation of code

--- UserAndPrivileges.sql
+++ UserAndPrivileges.sql
@@ -1,18 +1,18 @@
-/*
-	MariaDB
-    Versao 10.1.13
-    Rodrigo Luiz Barbosa
-	14/10/2016
+/*a
+MariaDB
+Versao 10.1.13
+Rodrigo Luiz Barbosa
+14/10/2016
 */
 /*
-	Select your database target.
-    DATABASE = Set name of your data base target. 
+Select your database target.
+DATABASE = Set name of your data base target. 
 */
 USE DATABASE;
 /* Create user of databese. */
 /* 
-	USERNAME = your user name
-    PASSAWORD = your password
+USERNAME = your user name
+PASSAWORD = your password
 */
 DROP USER IF EXISTS USERNAME;
 CREATE USER 'USERNAME'@'localhost' IDENTIFIED BY 'PASSWORD';
